f82b5dbfecffab7aa01f42c37bbc04e6ac94c38d
xozu <xozuhanjan@gmail.com>
2021-01-18 23:21:29 +0900

    Sign In form

--- client/src/styles.js
+++ client/src/styles.js
@@ -1,23 +1,5 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
-    appBar: {
-        borderRadius: 15,
-        margin: "30px 0",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    heading: {
-        color: "rgba(0, 180, 250, 1)",
-    },
-    image: {
-        marginLeft: "15px",
-    },
-    [theme.breakpoints.down("sm")]: { /* small 이하로는 flexDirection을 column-reverse로 하라. */
-        mainContainer: {
-            flexDirection: "column-reverse",
-        },
-    }
+    
 }));
